0b65e368d958e5f7bcea01a8e163a86352eaa134
unknown <georgekal123@gmail.com>
2018-11-22 16:10:36 +0200

    Fixing main script.

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,16 +1,16 @@
-import discord, os, asyncio, time, random
+import discord, os, asyncio, time, random, platform
 from discord.ext.commands import Bot
 from discord.ext import commands
 
 client= commands.Bot(command_prefix = "!")
 
-if not discord.opus.is_loaded():
+#if not discord.opus.is_loaded():
     # the 'opus' library here is opus.dll on windows
     # or libopus.so on linux in the current directory
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+#    discord.opus.load_opus('opus')
 	
 #Classes
 
@@ -286,6 +286,9 @@ async def on_ready():
     clog(client.user.name)
     clog(client.user.id)
     clog('------')
+	clog(platform.machine())
+	clog(platform.system())
+	clog(platform.platform())
     await client.change_presence(game=discord.Game(name="with someone's dick", type=1))
 
 
