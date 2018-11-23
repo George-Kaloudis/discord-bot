813d884c80c4041f61b8be8c8f31139099bc1ce1
unknown <georgekal123@gmail.com>
2018-11-24 01:37:59 +0200

    Fixing bugs.

--- DiscordBot.py
+++ DiscordBot.py
@@ -323,7 +323,7 @@ async def on_member_remove(member):
     emb=discord.Embed(description=member.mention + " " + str(member), color=0xdd10dd, timestamp=datetime.datetime.now())
     emb.set_author(name="Member Left", icon_url=member.avatar_url)
     emb.set_footer(text=("ID: " + str(member.id)))
-    await bot.send_message(ch, embed=emb)
+    await client.send_message(ch, embed=emb)
     
 @client.event
 async def on_ready():
