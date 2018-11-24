4eeef16b46e855ce3fdf78400873b9f5b79a1db9
unknown <georgekal123@gmail.com>
2018-11-24 21:55:12 +0200

    Trying to fix some things.

--- DiscordBot.py
+++ DiscordBot.py
@@ -374,14 +374,16 @@ async def on_member_update(before, after):
     member = before
     ser = member.server
     ch = findChannel(ser.channels, "bot")
-    for roleb in before.roles:
-        for rolea in after.roles:
-            if rolea not in before.roles:
+    broles = before.roles[1:]
+    aroles = after.roles[1:]
+    for roleb in broles:
+        for rolea in aroles:
+            if rolea not in broles:
                 emb=discord.Embed(description =  str(before.mention) + "**was given the " + str(rolea) + "role**" , color=0xdd10dd, timestamp=datetime.datetime.now())
                 emb.set_author(name=str(member), icon_url=member.avatar_url)
                 emb.set_footer(text=("ID: " + str(member.id)))
                 await client.send_message(ch, embed=emb)
-            if roleb not in after.roles:
+            if roleb not in aroles:
                 emb=discord.Embed(description =  str(before.mention) + "**was removed from the " + str(rolea) + "role**" , color=0xdd10dd, timestamp=datetime.datetime.now())
                 emb.set_author(name=str(member), icon_url=member.avatar_url)
                 emb.set_footer(text=("ID: " + str(member.id)))
