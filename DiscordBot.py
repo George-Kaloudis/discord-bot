37fd7f07511dc66ce050635fb1065252676c6417
unknown <georgekal123@gmail.com>
2018-11-24 02:26:03 +0200

    Fixing bugs.

--- DiscordBot.py
+++ DiscordBot.py
@@ -330,8 +330,7 @@ async def on_message_delete(message):
     ch = discord.utils.get(client.get_all_channels(), name='bot')
     
 	
-    emb=discord.Embed(description = "**Message sent by " + str(member.mention)  + "deleted in " + str(message.channel.mention) + "**", color=0xdd10dd, timestamp=datetime.datetime.now())
-    emb.add_field(value=message.content[:])
+    emb=discord.Embed(description = "**Message sent by " + str(member.mention)  + "deleted in " + str(message.channel.mention) + "**\n" + message.content[:] , color=0xdd10dd, timestamp=datetime.datetime.now())
     emb.set_author(name=str(member), icon_url=member.avatar_url)
     emb.set_footer(text=("ID: " + str(member.id)))
     await client.send_message(ch, embed=emb)
